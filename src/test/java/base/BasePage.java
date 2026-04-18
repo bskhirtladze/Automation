8d8927163ec065896dd351cd73ceb6904bdd808c
bskhirtladze <bach.skhirtladze@gmail.com>
2026-04-18 23:18:49 +0400

    run pipeline

--- src/test/java/base/BasePage.java
+++ src/test/java/base/BasePage.java
@@ -1,12 +1,11 @@
 package base;
 
 
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
 import org.openqa.selenium.*;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
-
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import java.time.Duration;
 import java.util.List;
 
--- src/test/java/base/BaseTest.java
+++ src/test/java/base/BaseTest.java
@@ -1,7 +1,6 @@
 package base;
 
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
+
 import org.openqa.selenium.WebDriver;
 import org.testng.ITestResult;
 import org.testng.annotations.AfterMethod;
