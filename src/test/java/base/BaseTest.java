0b7145d86bf64975b305d44ca19ed52923ac006a
bskhirtladze <bach.skhirtladze@gmail.com>
2026-04-18 23:22:36 +0400

    run pipeline

--- src/test/java/base/BaseTest.java
+++ src/test/java/base/BaseTest.java
@@ -1,6 +1,7 @@
 package base;
 
-
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.openqa.selenium.WebDriver;
 import org.testng.ITestResult;
 import org.testng.annotations.AfterMethod;
